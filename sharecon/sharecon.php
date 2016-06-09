6fb08760f3af8919c89c12a8a9da0b8f378492f1
Lukas <mrskigh@gmail.com>
2016-06-09 19:02:31 +0200

    1

--- sharecon/sharecon.php
+++ sharecon/sharecon.php
@@ -1,35 +1,31 @@
 <?php
+
+/**
+ *
+ * Name: SharEcon Plugin
+ * Description: to be updated
+ * Version: 0.1
+ * Author: Lukas Adrian
+ *
+*/
+
 function sharecon_load(){
-	Zotlabs\Extend\Hook::register('post_local', 'addon/sharecon/sharecon.php', 'test');
 }
 
 function sharecon_unload(){
-	Zotlabs\Extend\Hook::unregister('post_local', 'addon/sharecon/sharecon.php', 'test');
 }
 
-function test(){
+function sharecon_module(){}
 
-	/**
-	 *
-	 * OK, we're allowed to do our stuff.
-	 * Here's what we are going to do:
-	 * load the list of timezone names, and use that to generate a list of world cities.
-	 * Then we'll pick one of those at random and put it in the "location" field for the post.
-	 *
-	 */
-
-	$cities = array();
-	$zones = timezone_identifiers_list();
-	foreach($zones as $zone) {
-		if((strpos($zone,'/')) && (! stristr($zone,'US/')) && (! stristr($zone,'Etc/')))
-			$cities[] = str_replace('_', ' ',substr($zone,strpos($zone,'/') + 1));
-	}
-
-	if(! count($cities))
-		return;
-	$city = array_rand($cities,1);
-	$item['location'] = $cities[$city];
-
-	return;
+function sharecon_content(){
+	$output = <<< SB
+	
+	<table style='border:1 solid black;'><tr>
+		<td>Artikelname</td><td>Hammer</td>
+	</tr><tr>
+		<td>Beschreibung</td><td>Gr0ßer Zimmermannshammer. Länge: 30cm Gewicht: 850g</td>
+	</tr></table>
+	
+SB;
 }
 ?>
