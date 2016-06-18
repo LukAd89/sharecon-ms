6102f6fd0f5fbe1327a4f86d4d87d243f5ea7e2c
MrSkigh <mrskigh@gmail.com>
2016-06-18 20:01:24 +0200

    Included first database access

---
+++ sharingecon/db_functions.php
@@ -0,0 +1,71 @@
+<?php
+if (isset($_POST['function'])) {
+	if($_POST['function'] == "addNewShare"){
+		echo addNewShare();
+	}
+	else if($_POST['function'] == "loadShares"){
+		echo loadShares();
+	}
+}
+
+function addNewShare(){
+	$title = strip_tags($_POST['inputTitle']);
+	$shortdesc = strip_tags($_POST['inputShortDesc']);
+		
+	//return($title . $shortdesc);
+	
+	$server = "localhost";
+	$user = "root";
+	$password = "";
+	$dbname = "hz_sharecon";
+	
+	$conn = new mysqli($server, $user, $password, $dbname);
+	
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql_query = "INSERT INTO sharedobjects (title, shortdesc) VALUES ('" . $title . "', '" . $shortdesc . "')";
+	
+	if ($conn->query($sql_query) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql_query . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+}
+
+function loadShares(){
+	
+    $json[] = array
+    (
+        'message' => 'Not Requested'
+    );
+	//return json_encode($json);
+	
+	$server = "localhost";
+	$user = "root";
+	$password = "";
+	$dbname = "hz_sharecon";
+	
+	$conn = new mysqli($server, $user, $password, $dbname);
+	
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$resArray = array();
+	$sql_query = "SELECT * FROM sharedobjects";
+	
+	if($result = $conn->query($sql_query)){
+		while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+				$resArray[] = $row;
+		}
+		echo json_encode($resArray);
+	}
+
+	$result->close();
+	$conn->close();
+}
+?>
---
+++ sharingecon/main_js.js
@@ -0,0 +1,57 @@
+
+
+function test(){
+	console.log("This is a Test");
+}
+
+$(function(){
+	/* Called when Submit button of Add New Share form is clicked */
+	$("#btn_addNewShare").click(function(){
+		addNewShare();
+	});
+});
+
+$(function(){
+	$('[href=#tab-find-shares]').on('shown.bs.tab', function(event){
+		loadShares();
+	});
+});
+
+function addNewShare(){
+	$.ajax({
+		type : "POST",
+		url : "db_functions.php",
+		data : $("#form_addNewShare").serialize() + "&function=addNewShare",
+		success : function(msg){
+			console.log(msg);
+			$("#modal_addNewShare").modal('hide');
+		},
+		error : function(){
+			console.log("error");
+		}
+	});
+}
+
+function loadShares(){
+	$.ajax({
+		type : "POST",
+		url : "db_functions.php",
+		data : {function : "loadShares"},
+		success : function(msg){
+			var jsonObj = jQuery.parseJSON(msg);
+			
+			$('#tab-find-shares-content').html("");
+			
+			for(i=0; i<jsonObj.length; i++){
+				$('#tab-find-shares-content').append('<div class="panel panel-default">' + 
+					'<div class="panel-heading"><i class="fa fa-file"></i>' + jsonObj[i].Title + '</div>' + 
+					'<div class="panel-body"><div class="media"><div class="media-left"><img class="media-object" src="standard.png" alt="..."></div><div class="media-body"><div class="well">' + jsonObj[i].ShortDesc + '</div></div></div></div>' + 
+					'<div class="panel-footer"><div class="row"></div></div>' + 
+					'</div>');
+			}
+		},
+		error : function(){
+			console.log("error");
+		}
+	});
+}
