a7ad530574d9c39fae112dae5a3c7d230e7d0bf9
MrSkigh <mrskigh@gmail.com>
2016-06-18 12:45:30 +0200

    New Template2

--- sharingecon/sharingecon.php
+++ sharingecon/sharingecon.php
@@ -21,18 +21,7 @@ function sharingecon_module() {}
 
 function sharingecon_content(&$a) {
 
-$header = t('QR Generator');
-$prompt = t('Enter some text');
 
-$o .= <<< EOT
-<h2>$header</h2>
-
-<div>$prompt</div>
-<input type="text" id="qr-input" onkeyup="makeqr();" />
-<div id="qr-output"></div>
-
-
-EOT;
-return $o;
+return file_get_contents("main_page");
 
 }
