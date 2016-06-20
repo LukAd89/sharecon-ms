1e5ccd5c687e22f4d2445e12a6b3e3e36db13b89
MrSkigh <mrskigh@gmail.com>
2016-06-20 17:57:52 +0200

    1

--- sharingecon/main_js.js
+++ sharingecon/main_js.js
@@ -57,8 +57,8 @@ function loadShares(){
 			for(i=0; i<jsonObj.length; i++){
 				$('#tab-find-shares-content').append('<div class="panel panel-default panel-share-object">' + 
 					'<div class="panel-heading">' + jsonObj[i].Title + '</div>' + 
-					'<div class="panel-body"><div class="media"><div class="media-left"><img class="media-object thumbnail" src="addon/sharecon/standard.png" alt="..."></div><div class="media-body"><div class="well">' + jsonObj[i].ShortDesc + '</div></div></div></div>' + 
-					'<div class="panel-footer"><div class="row"></div></div>' + 
+					'<div class="panel-body"><div class="media"><div class="media-left"><img class="media-object thumbnail" src="addon/sharingecon/standard.png" alt="..."></div><div class="media-body"><div class="well">' + jsonObj[i].ShortDesc + '</div></div></div></div>' + 
+					'<div class="panel-footer"><div class="row"><div class="col-md-12 text-right"><div class="btn-group"><button type="button" class="btn btn-default"><span class"glyphicon glyphicon-info-sign"></span> More Details</button><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-envelope"></span> Write Message</button></div></div></div></div>' + 
 					'</div>');
 			}
 		},
