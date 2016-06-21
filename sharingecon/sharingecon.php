1c949574f0666a7f1ac0410cf537a9f435f04cbb
MrSkigh <mrskigh@gmail.com>
2016-06-21 17:09:40 +0200

    1

--- sharingecon/sharingecon.php
+++ sharingecon/sharingecon.php
@@ -27,7 +27,7 @@ if(isset($_POST['function'])){
 }
 else {
 	$data = array(
-				"owner" => "1",
+				"owner" => App::$channel['channer_guid'],
 				"title" => strip_tags($_POST['input-title']),
 				"shortdesc" => strip_tags($_POST['input-short-desc'])
 			);
