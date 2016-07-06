897b0b182a64ae0e05252b813f155c1ebde4abed
MrSkigh <mrskigh@gmail.com>
2016-07-06 15:16:07 +0200

    1

--- sharingecon/sharingecon.php
+++ sharingecon/sharingecon.php
@@ -12,12 +12,21 @@
 
 
 
-//require_once('include/enotify.php');
+require_once('include/enotify.php');
 
 function sharingecon_module() {}
 
+function sharingecon_plugin_admin(&$a, &$o) {
+
+	$o = '<div></div>&nbsp;&nbsp;&nbsp;&nbsp;<a href="' . z_root() . '/hubwall">' . t('Send email to all members') . '</a></br/>';
+
+}
+
+function sharingecon_post(&$a) {
+}
+
 function sharingecon_content(&$a) {
 
-	return "hello";
+	return 'hello';
 
 }
