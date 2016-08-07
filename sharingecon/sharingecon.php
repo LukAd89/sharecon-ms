dc5897e2e3252b4ab52d1602752041e2868bfdac
MrSkigh <mrskigh@gmail.com>
2016-08-07 15:41:20 +0200

    1

--- sharingecon/sharingecon.php
+++ sharingecon/sharingecon.php
@@ -70,9 +70,9 @@ function get_shares_list($args){
 	$result = "";
 	for($i=0; $i<count($data); $i++){
 		
-		//if($data[$i]['Imagename'] === NULL){
+		if($data[$i]['Imagename'] === NULL){
 			$data[$i]['Imagename'] ='default.jpg';
-		//}
+		}
 		
 		if($args['ownerview']){
 			$status='';
@@ -85,6 +85,7 @@ function get_shares_list($args){
 			'$shareid' 		=> $data[$i]['ID'],
 			'$title' 		=> $data[$i]['Title'],
 			'$shortdesc' 	=> $data[$i]['ShortDesc'],
+			'$imagename'	=> $data[$i]['Imagename'],
 			'$checked'		=> $status
 			));
 		}
