8312426abc9b2bd7cd5e5867b6a1122d98f5b48d
MrSkigh <mrskigh@gmail.com>
2016-09-05 20:09:11 +0200

    1

--- sharingecon/functions.php
+++ sharingecon/functions.php
@@ -489,7 +489,7 @@ function get_Distance($customerid, $shareid){
 	
 	$jsonresult = json_decode($curlresult, true);
 	
-	return var_dump($jsonresult['rows'][0]['elements']);
-	return $jsonresult['rows']['elements']['distance']['text'];
+	//return var_dump($jsonresult['rows'][0]['elements']);
+	return $jsonresult['rows'][0]['elements'][0]['distance']['text'];
 }
 ?>
