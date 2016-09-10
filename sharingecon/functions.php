fdc4e29144571e68bd61ef2ec3c9c83798b011b5
MrSkigh <mrskigh@gmail.com>
2016-09-10 17:02:12 +0200

    1

--- sharingecon/functions.php
+++ sharingecon/functions.php
@@ -31,6 +31,7 @@ function get_SharesList($args){
 		$shareids[] = $dataval['ID'];
 	}
 	$distances = get_MultipleDistances(App::$channel['channel_hash'], $shareids);
+	var_dump($distances);
 	for($i=0; $i<count($data); $i++){
 		$dataval[$i] -> distance = $distances[$i];
 	}
