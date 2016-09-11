a3dc9c21d518fc16bc5f50e70e714b6dd0bc068b
MrSkigh <mrskigh@gmail.com>
2016-09-11 19:33:56 +0200

    1

--- sharingecon/admin_js.js
+++ sharingecon/admin_js.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 	$('#new-branch-modal').on('show.bs.modal', function (e) {
 		var branchid = $(e.relatedTarget).data('branchid');
-		$('#input-branch-id').val(branchid);
+		$('#new-branch-modal #input-branch-id').val(branchid);
 	});
 	
 	$('#edit-branch-modal').on('show.bs.modal', function (e) {
 		var branchid = $(e.relatedTarget).data('branchid');
-		$('#input-branch-id').val(branchid);
-		$('#input-parent').val($('#tr_' + branchid + ' .td_parent').text());
-		$('#input-title').val($('#tr_' + branchid + ' .td_title').text());
-		$('#input-tags').val($('#tr_' + branchid + ' .td_tags').text());
+		$('#edit-branch-modal #input-branch-id').val(branchid);
+		$('#edit-branch-modal #input-parent').val($('#edit-branch-modal #tr_' + branchid + ' .td_parent').text());
+		$('#edit-branch-modal #input-title').val($('#edit-branch-modal #tr_' + branchid + ' .td_title').text());
+		$('#edit-branch-modal #input-tags').val($('#edit-branch-modal #tr_' + branchid + ' .td_tags').text());
 	});
 });
 
