2306fe9ed5cd1ce292dcaa5bb62a987ec07661e3
MrSkigh <mrskigh@gmail.com>
2016-09-11 19:27:06 +0200

    1

--- sharingecon/admin_js.js
+++ sharingecon/admin_js.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
 		$('#input-branch-id').val(branchid);
 	});
 	
-	$('edit-branch-modal').on('show.bs.modal', function (e) {
+	$('#edit-branch-modal').on('show.bs.modal', function (e) {
 		var branchid = $(e.relatedTarget).data('branchid');
-		console.log(branchid);
 		$('#input-branch-id').val(branchid);
 		$('#input-parent').val($('#tr_' + branchid + ' .td_parent'));
 		$('#input-title').val($('#tr_' + branchid + ' .td_title'));
