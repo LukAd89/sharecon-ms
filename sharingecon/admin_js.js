1d051465806a32d8b30a1fbf9f30a02810f35d1c
MrSkigh <mrskigh@gmail.com>
2016-09-11 19:52:50 +0200

    1

--- sharingecon/admin_js.js
+++ sharingecon/admin_js.js
@@ -16,9 +16,24 @@ $(document).ready(function(){
 		var branchid = $(e.relatedTarget).data('branchid');
 		$('#delete-branch-modal #input-branch-id').val(branchid);
 	});
+	
+	$("#new-branch-submit").click(function(){
+		var parent = $('#new-branch-modal #input-branch-id');
+		var title = $('#new-branch-modal #input-title');
+		var tags = $('#new-branch-modal #input-tags');
+		console.log("New: " + parent + "  :  " + title + "  :  " + tags);
+	});
+	
+	$("#edit-branch-submit").click(function(){
+		var branch = $('#edit-branch-modal #input-branch-id');
+		var parent = $('#edit-branch-modal #input-parent');
+		var title = $('#edit-branch-modal #input-title');
+		var tags = $('#edit-branch-modal #input-tags');
+		console.log("Edit: " + branch + "  :  " + parent + "  :  " + title + "  :  " + tags);
+	});
+	
+	$("#new-branch-submit").click(function(){
+		var branch = $('#new-branch-modal #input-branch-id');
+		console.log("Delete" + branch);
+	});
 });
-
-function addNewBranch(parentid){
-	$("#new-branch-modal").modal("show");
-	console.log(parentid);
-}
