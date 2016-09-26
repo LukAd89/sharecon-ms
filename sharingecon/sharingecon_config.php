b9b697b5f6f37ab1f16c8cc4986e40a7c22ddda6
MrSkigh <mrskigh@gmail.com>
2016-09-26 20:29:28 +0200

    1

--- sharingecon/sharingecon_config.php
+++ sharingecon/sharingecon_config.php
@@ -6,3 +6,5 @@ define('SERVER_DBNAME', "hz_sharecon");
 define('SERVER_HUB_DBNAME', "hubzilla");
 define('SITE_ROOT', realpath(dirname(__FILE__)));
 define('GOOGLEAPI_KEY', 'AIzaSyDHMXzuvg32pFTh2XTvps89IoAOgzcaOzU');
+define('TREE_WEIGHT_SPECIAL' , 1);
+define('TREE_WEIGHT_GENERAL' , 1);
